034bc5d4c19ef6d8d04f249df3a687ef627b15a3
Zuofu Li <zuofu@ualberta.ca>
2022-07-05 16:04:12 -0600

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image,  StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={{ width: 305, height: 159 }} /> 
+
+      <Text style={{color: '#888', fontSize: 18}}> 
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
     </View>
   );
 }
